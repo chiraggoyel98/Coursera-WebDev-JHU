c78310d5c644445a3d082a41a38c1114b029dbc6
Chirag <chiraggoyel98@gmail.com>
2021-08-06 20:51:10 +0530

    unneccessary code was removed

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function () {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
 for (var i =0 ; i < names.length ; i++) {
@@ -14,5 +14,5 @@ for (var i =0 ; i < names.length ; i++) {
 
 }
 
-})(window);
+})();
 
