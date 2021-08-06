899b47b6ce78a1e3352cf31667febf4131884c68
Chirag <chiraggoyel98@gmail.com>
2021-08-06 20:48:58 +0530

    Changes if cond

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,9 +3,9 @@
     
 for (var i =0 ; i < names.length ; i++) {
 
- var firstLetter = names[i].charAt(0)
+ var firstLetter = names[i].charAt(0).toLowerCase();
 
- if(firstLetter === 'j' || firstLetter === 'J') {
+ if(firstLetter === 'j' ) {
     byeSpeaker.speak(names[i]);
  }
  else {
